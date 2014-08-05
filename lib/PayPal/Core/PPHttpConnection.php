1678653d94d4b2eb3e24d28d4222faa883a96632
Craig <craigdataqa@gmail.com>
2014-08-05 14:05:16 -0400

    Update PPHttpConnection.php

--- lib/PayPal/Core/PPHttpConnection.php
+++ lib/PayPal/Core/PPHttpConnection.php
@@ -60,6 +60,8 @@ class PPHttpConnection
 		switch($this->httpConfig->getMethod()) {
 			case 'POST':
 				curl_setopt($ch, CURLOPT_POST, true);
+			case 'PUT':
+			case 'PATCH':
 				curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 				break;
 		}
